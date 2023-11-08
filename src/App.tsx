f21def808f617e70bca846b68a9124aad569f427
임준수 <yimj57027@hotmail.com>
2023-11-08 15:04:22 +0900

    d

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { disableBodyScroll } from "body-scroll-lock";
 import { useEffect } from "react";
 
 function App() {
-  const body = document.querySelector("body") as HTMLElement;
-  // const root = document.querySelector("root") as HTMLElement
+  // const body = document.querySelector("body") as HTMLElement;
+  const root = document.querySelector("root") as HTMLElement
   useEffect(() => {
-    disableBodyScroll(body);
-    // disableBodyScroll(root);
+    // disableBodyScroll(body);
+    disableBodyScroll(root);
   }, []);
 
   return (
